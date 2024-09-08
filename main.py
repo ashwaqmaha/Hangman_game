6c7097225780a88199fe9a3d8a2292ca3b4e189d
Amahamed <ashwaqmahamed1@gmail.com>
2024-09-08 23:57:49 +0200

    updating hangman phase 1

--- main.py
+++ main.py
@@ -1,5 +1,62 @@
 import random
 
+strikes = ["""
+/----
+|   0
+|
+|   
+|  
+_______
+""",
+"""
+/----
+|   0
+|   |
+|
+|
+_______
+""",
+"""
+/----
+|   0
+|  /|
+|
+|
+_______
+""",
+"""
+/----
+|   0
+|  /|\\
+|   
+|
+_______
+""",
+"""
+/----
+|   0
+|  /|\\
+|   |
+|  
+_______
+""",
+"""
+/----
+|   0
+|  /|\\
+|   |
+|  / 
+_______
+""",
+"""
+/----
+|   0
+|  /|\\
+|   |
+|  / \\
+_______
+"""]
+
 def get_words(file_name):
     with open(file_name,"r") as file:
         words = file.readlines()
@@ -26,12 +83,12 @@ def check_answer_correctness(answer, random_word, random_letter_index):
         print(f"The word was: {random_word}\n\nWrong! Do better next time.")
 
 def hangman_start():
-    word_file = input("Words file? [leave empty to use short_words.txt] : ")
+    word_file = input("Words file? [empty = short_words.txt, 1 = complex_words.txt]: ")
 
     if len(word_file)== 0:
-        word_file = "short_words.txt"
+        word_file = "word_collections/short_words.txt"
     else:
-        word_file = "complex_words.txt"
+        word_file = "word_collections/complex_words.txt"
     
     words = get_words(word_file)
 
@@ -40,6 +97,7 @@ def hangman_start():
     print(f"Guess the word: {mystery_word}\n")
     answer = input("Guess the missing letter: ")
     check_answer_correctness(answer,random_word,random_letter_index)
+    print(strikes[4])
 
 if __name__ == "__main__":
     hangman_start()
