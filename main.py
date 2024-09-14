5bb5d8248a086a57cbb48d04b8e008a8413fb3ff
Amahamed <ashwaqmahamed1@gmail.com>
2024-09-14 21:49:58 +0200

    Add logic to reveal one letter in mystery word

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import random
 from game_manager import check_answer_correctness
-from mystery_word_generator import choose_random_missing_letter, choose_random_word
+from mystery_word_generator import choose_random_missing_letters, choose_random_word
 from word_database_manager import get_words
 
 
@@ -14,10 +15,11 @@ def hangman_start():
     words = get_words(word_file)
 
     random_word = choose_random_word(words)
-    mystery_word, random_letter_index= choose_random_missing_letter(random_word)
+    mystery_word, random_word= choose_random_missing_letters(random_word)
     print(f"Guess the word: {mystery_word}\n")
-    answer = input("Guess the missing letter: ")
-    check_answer_correctness(answer,random_word,random_letter_index)
+    print(random_word)
+    # answer = input("Guess the missing letter: ")
+    # check_answer_correctness(answer,random_word,random_word)
 
 if __name__ == "__main__":
     hangman_start()
--- mystery_word_generator.py
+++ mystery_word_generator.py
@@ -5,9 +5,9 @@ def choose_random_word(words):
     random_word = words[random_word_index].rstrip()
     return random_word
 
-def choose_random_missing_letter(random_word):
-    random_letter_index = random.randrange(len(random_word)-1)
-    random_word.replace(random_word[random_letter_index],"_")
-    mystery_word = random_word.replace(random_word[random_letter_index],"_")
-    return mystery_word, random_letter_index
+def choose_random_missing_letters(random_word):
+    random_letter_index = random.randrange(len(random_word))
+    mysteryword = [char if char == random_word[random_letter_index] else "_" for char in random_word]
+    mystery_word = " ".join(mysteryword)
+    return mystery_word, random_word
 
