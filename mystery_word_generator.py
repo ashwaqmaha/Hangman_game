3c31c51dfad26d2b0858e94b2736b0e30628d569
Amahamed <ashwaqmahamed1@gmail.com>
2024-09-15 20:54:48 +0200

    modified choose_random_missing_letters to return mysteryword_list as well

--- mystery_word_generator.py
+++ mystery_word_generator.py
@@ -2,12 +2,12 @@ import random
 
 def choose_random_word(words):
     random_word_index = random.randrange(len(words)-1)
-    random_word = words[random_word_index].rstrip()
+    random_word = words[random_word_index].rstrip().lower()
     return random_word
 
 def choose_random_missing_letters(random_word):
     random_letter_index = random.randrange(len(random_word))
-    mysteryword = [char if char == random_word[random_letter_index] else "_" for char in random_word]
-    mystery_word = " ".join(mysteryword)
-    return mystery_word, random_word
+    mysteryword_list = [char if char == random_word[random_letter_index] else "_" for char in random_word]
+    mystery_word = " ".join(mysteryword_list)
+    return mystery_word, random_word, mysteryword_list
 
